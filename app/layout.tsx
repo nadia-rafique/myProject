7e6b7663e7805e6332d38b2329dff6f392adc028
Nadia Rafique <108391660+nadia-rafique@users.noreply.github.com>
2025-10-08 00:45:27 -0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,9 +5,11 @@ import { Analytics } from '@vercel/analytics/next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'Portfolio',
-  description: 'Created with v0',
+  title: 'Nadia Rafique | Senior Frontend Dev',
+  description: 'Front-End Developer with expertise in HTML, CSS, JavaScript, jQuery, Bootstrap, SASS, Tailwind CSS, React.js, and responsive frameworks. Passionate about creating responsive, user-friendly web designs, with a strong mobile-first approach and focus on accessibility and performance.',
   generator: 'v0.app',
+  keywords: 'Nadia Rafique, Frontend Developer, Senior Frontend Developer, Nadia Portfolio, nadiarafique1441@gmail.com',
+  
 }
 
 export default function RootLayout({
